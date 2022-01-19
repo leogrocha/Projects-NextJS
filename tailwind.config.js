3bedbdfbfb602e0c39bcb0b78ff8bb559770a8ee
Leonardo Rocha <leonaordoleo182@hotmail.com>
2022-01-18 21:57:04 -0300

    Add config TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  purge: {
+    content: [
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+       './src/components/**/*.{js,ts,jsx,tsx}'
+    ],
+    safelist: [
+      /^bg-/,
+      /^to-/,
+      /^from-/,
+    ]
+  },
+  darkMode: 'class', 
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
